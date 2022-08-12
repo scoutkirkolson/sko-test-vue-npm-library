798de2cfe1dd856211017b668cc9b225bcccdd2a
Louck Sitskoorn <louck@workspace.nl>
2022-08-12 15:40:08 +0200

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner>This is a bannereeeeeee!</Banner>
   </div>
 </template>
 
